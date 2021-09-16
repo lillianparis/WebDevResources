62665f63fd63e186cc2de1e8b4c561624192d1ab
Lillian Paris <lillian.paris529@gmail.com>
2021-09-16 14:08:52 -0700

    Added footer

--- webdev/src/Pages/About/index.js
+++ webdev/src/Pages/About/index.js
@@ -1,12 +1,14 @@
 import React from "react";
 import "materialize-css";
 import Header from "../../components/Navbar/navbar";
+import Foot from"../../components/Footer/footer";
 
 function About() {
   return (
     <>
       <Header />
       <h1> About page </h1>{" "}
+      <Foot />
     </>
   );
 }
--- webdev/src/Pages/Landing/index.js
+++ webdev/src/Pages/Landing/index.js
@@ -1,12 +1,14 @@
 import React from "react";
 import "materialize-css";
 import Header from "../../components/Navbar/navbar";
+import Foot from "../../components/Footer/footer"
 
 function Landing() {
   return (
     <>
       <Header />
       <h1> landing page </h1>{" "}
+      <Foot />
     </>
   );
 }
