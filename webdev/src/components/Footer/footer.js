b8bd47196fbb5826aded949658a6dc5a340e88c1
Lillian Paris <lillian.paris529@gmail.com>
2021-09-16 14:19:35 -0700

    Filled in Footer

--- webdev/src/components/Footer/footer.js
+++ webdev/src/components/Footer/footer.js
@@ -7,15 +7,16 @@ function Foot() {
     return(
         <Footer
         className="bottom-fixed"
-        copyrights="&copy 2015 Copyright Text"
+        copyrights="2021 Copyright Text"
         links={<ul><li><a className="grey-text text-lighten-3" href="#!">Link 1</a></li><li><a className="grey-text text-lighten-3" href="#!">Link 2</a></li><li><a className="grey-text text-lighten-3" href="#!">Link 3</a></li><li><a className="grey-text text-lighten-3" href="#!">Link 4</a></li></ul>}
-        moreLinks={<a className="grey-text text-lighten-4 right" href="#!">More Links</a>}
+        moreLinks={<a className="grey-text text-lighten-4 right" href="#!">Contact</a>}
       >
         <h5 className="white-text">
-          Footer Content
+          Web Development Resources
         </h5>
         <p className="grey-text text-lighten-4">
-          You can use rows and columns here to organize your footer content.
+          Here you can find contact information. This website uses React and Materialize.
+          Created by Daniel Molina-Power and Lillian Paris.
         </p>
       </Footer>
     );
