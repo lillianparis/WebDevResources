fcc3cd8aa88c1d7eb71503988a315b560b46e6e6
Dmolina-power <bigdpower@gmail.com>
2021-09-16 12:32:03 -0700

    fixed image and added to navbar

--- webdev/src/Pages/Landing/index.js
+++ webdev/src/Pages/Landing/index.js
@@ -1,14 +1,14 @@
 import React from "react";
 import "materialize-css";
-import Header from "../../components/navbar"
+import Header from "../../components/Navbar/navbar";
 
 function Landing() {
-    return ( 
-        <>
-        <Header />
-        <h1>landing page</h1>
-        </>
-    )
+  return (
+    <>
+      <Header />
+      <h1> landing page </h1>{" "}
+    </>
+  );
 }
 
 export default Landing;
