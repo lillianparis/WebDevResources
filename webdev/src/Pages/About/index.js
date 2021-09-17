ecb2b5c5740e24cca83030277d3103dde1874668
Dmolina-power <bigdpower@gmail.com>
2021-09-17 13:13:15 -0700

    added team json and team card about me started

--- webdev/src/Pages/About/index.js
+++ webdev/src/Pages/About/index.js
@@ -1,16 +1,51 @@
-import React from "react";
+import React, { Component } from "react";
 import "materialize-css";
 import Header from "../../components/Navbar/navbar";
-import Foot from"../../components/Footer/footer";
+import Foot from "../../components/Footer/footer";
+import TeamCard from "../../components/TeamCard";
+import Grid from "@material-ui/core/Grid";
+import teamProfiles from "../../TeamProfile.json";
 
-function About() {
-  return (
-    <>
-      <Header />
-      <h1> About page </h1>
-      <Foot />
-    </>
-  );
+export class About extends Component {
+  state = {
+    teamProfiles,
+  };
+  render() {
+    return (
+      <>
+        <Header />
+        <Grid
+          container
+          direction="row"
+          justify="center"
+          alignItems="center"
+          spacing={10}
+          style={{ paddingLeft: "25px", padding: "35px" }}
+        >
+          {this.state.teamProfiles.map((teamProfile) => (
+            <Grid
+              key={teamProfiles.id}
+              item
+              xs={12}
+              sm={6}
+              md={6}
+              lg={4}
+              xl={3}
+            >
+              <TeamCard
+                id={teamProfile.id}
+                key={teamProfile.id}
+                name={teamProfile.content}
+                image={teamProfile.image}
+                content={teamProfile.name}
+                link={teamProfile.link}
+              />{" "}
+            </Grid>
+          ))}{" "}
+        </Grid>{" "}
+        <Foot />
+      </>
+    );
+  }
 }
-
 export default About;
