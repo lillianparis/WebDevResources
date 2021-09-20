37e8eb0faa3e5076aa2fa789151a9e839744275b
Lillian Paris <lillian.paris529@gmail.com>
2021-09-20 12:55:48 -0700

    Added goodreads, not functional

--- webdev/src/App.js
+++ webdev/src/App.js
@@ -9,6 +9,7 @@ import Landing from "./Pages/Landing";
 import About from "./Pages/About";
 import Resources from "./Pages/Resources";
 import Notes from "./Pages/Notes";
+import goodreads from "./Pages/goodreads"
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
       <Route exact path="/about" component={About} />
       <Route exact path="/resources" component={Resources} />
       <Route exact path="/notes" component={Notes} />
+      <Route exact path="/goodreads" component={goodreads} />
     </Router>
   );
 }
---
+++ webdev/src/Pages/goodreads/index.js
@@ -0,0 +1,33 @@
+// import React from "react";
+// import "materialize-css";
+// import Header from "../../components/Navbar/navbar";
+
+
+// function books(){
+//     return(
+
+//         <Row>
+//                     <Header />
+//   <Col
+//     m={6}
+//     s={12}
+//   >
+//     <Card
+//       closeIcon={<Icon>close</Icon>}
+//       header={<CardTitle image="https://materializecss.com/images/sample-1.jpg" reveal waves="light"/>}
+//       reveal={<p>Here is some more information about this product that is only revealed once clicked on.</p>}
+//       revealIcon={<Icon>more_vert</Icon>}
+//       title="Card Title"
+//     >
+//       <p>
+//         <a href="#">
+//           This is a link
+//         </a>
+//       </p>
+//     </Card>
+//   </Col>
+// </Row>
+//     )
+// }
+
+// export default books;
