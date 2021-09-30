037d0413a262bea9a28fa25044636864c984eba9
Lillian Paris <lillian.paris529@gmail.com>
2021-09-30 10:40:50 -0700

    Formatting

--- webdev/src/Pages/Landing/index.js
+++ webdev/src/Pages/Landing/index.js
@@ -8,38 +8,38 @@ import Grid from "@material-ui/core/Grid";
 
 export class Landing extends Component {
 
-  render (){
-  return (
-    <>
-      <Header />
-      <Grid
-        container
-        direction="row"
-        justify="center"
-        alignItems="center"
-        spacing={10}
-        style={{ paddingLeft: "25px", padding: "35px" }}
-      >
-
+  render() {
+    return (
+      <>
+        <Header />
         <Grid
-    
-          item
-          xs={12}
-          sm={6}
-          md={6}
-          lg={4}
-          xl={3}
+          container
+          direction="row"
+          justify="center"
+          alignItems="center"
+          spacing={10}
+          style={{ paddingLeft: "25px", padding: "35px" }}
         >
-<defaultCard   />
-     
-      
+
+          <Grid
+
+            item
+            xs={12}
+            sm={6}
+            md={6}
+            lg={4}
+            xl={3}
+          >
+            <defaultCard />
+
+
+          </Grid>
         </Grid>
-      </Grid>
-    
-      <Foot />
-    </>
-  );
-};
+
+        <Foot />
+      </>
+    );
+  };
 }
 
 export default Landing;
