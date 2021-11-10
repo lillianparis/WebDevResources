b02cad234e70c419b0bfe175d2146565a43668ab
Lillian Paris <lillian.paris529@gmail.com>
2021-11-10 11:58:25 -0700

    Fized error

--- webdev/src/Pages/goodreads/index.js
+++ webdev/src/Pages/goodreads/index.js
@@ -1,47 +1,47 @@
-import React from "react";
-import "materialize-css";
-import Header from "../../components/Navbar/navbar";
-import revealCard from "../../components/revealCard";
-import Grid from "@material-ui/core/Grid";
-import bookCards from "../../bookCards.json"
+// import React from "react";
+// import "materialize-css";
+// import Header from "../../components/Navbar/navbar";
+// import revealCard from "../../components/revealCard";
+// import Grid from "@material-ui/core/Grid";
+// import bookCards from "../../bookCards.json"
 
-function books(){
-  state = {
-    bookCards,
-  };
-    return(
-        <>
-<Header />
-        <Grid
-        container
-        direction="row"
-        justify="center"
-        alignItems="center"
-        spacing={10}
-        style={{ paddingLeft: "25px", padding: "35px" }}
-      >
+// function books(){
+//   state = {
+//     bookCards,
+//   };
+//     return(
+//         <>
+// <Header />
+//         <Grid
+//         container
+//         direction="row"
+//         justify="center"
+//         alignItems="center"
+//         spacing={10}
+//         style={{ paddingLeft: "25px", padding: "35px" }}
+//       >
         
-          <Grid
-            key={bookCards.id}
-            item
-            xs={12}
-            sm={6}
-            md={6}
-            lg={4}
-            xl={3}
-          >
-       <TeamCard
-                id={teamProfile.id}
-                key={teamProfile.id}
-                name={teamProfile.content}
-                image={teamProfile.image}
-                content={teamProfile.name}
-                link={teamProfile.link}
-              />{" "}
-          </Grid>
-          {" "}
-          </Grid>}{" "}
-          </>
-    )};
+//           <Grid
+//             key={bookCards.id}
+//             item
+//             xs={12}
+//             sm={6}
+//             md={6}
+//             lg={4}
+//             xl={3}
+//           >
+//        <TeamCard
+//                 id={teamProfile.id}
+//                 key={teamProfile.id}
+//                 name={teamProfile.content}
+//                 image={teamProfile.image}
+//                 content={teamProfile.name}
+//                 link={teamProfile.link}
+//               />{" "}
+//           </Grid>
+//           {" "}
+//           </Grid>}{" "}
+//           </>
+//     )};
 
-export default books;
+// export default books;
